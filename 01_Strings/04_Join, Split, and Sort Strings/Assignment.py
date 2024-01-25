2d44a301be976cb5f86a76c5dd0485bc711738c7
reigna rodgers <rodgerei000@warren.k12.in.us>
2024-01-25 15:00:44 -0500

    U1L3 Assigment.py

--- 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
+++ 01_Strings/04_Join, Split, and Sort Strings/Assignment.py
@@ -1,10 +1,7 @@
-# Starter code
-# YOUDO:  create first_name and last_name variable and
-# assign values using the input function
-first_name = ""
-last_name = ""
+
+first_name = input("Enter your first name: ")
+last_name = input("Enter your last name: ")
 
 sep = " "
-full_name = ""  # YOUDO use the join function to
-# join together first_name and last_name
+full_name = sep.join([first_name, last_name])
 print(full_name)
