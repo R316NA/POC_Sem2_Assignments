8c16c22ba4e2d42b942cc35ee86fa1acee982ce8
reigna rodgers <rodgerei000@warren.k12.in.us>
2024-04-29 12:15:58 -0400

    assigment.py 08.5

--- 08_OOP/05_Methods/assignment.py
+++ 08_OOP/05_Methods/assignment.py
@@ -14,18 +14,11 @@ class Rectangle:
     def get_height(self) -> float:
         return self.__height
 
-    # YOUDO the get_base method
+     base = Rectangle.get_base()
 
     def get_perimeter(self) -> float:
         return 2 * self.__base + 2 * self.__height
-
-    # Youdo get_area method
-    
+      area = Rectangle.get_area()
     def __str__(self) -> str:
-        #Rectangle with base:3, height:4
         return "Rectangle with base:" + str(self.__base) + ", height:" + str(self.__height)
     
-
-
-# YOUDO>  create two rectangles.  print their base, height, perimeter, and area
-# using only the methods not the fields/property/attributes
